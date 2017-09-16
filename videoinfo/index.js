ea4fcda80ec6ff91bb8bf7909944d9e1afc540c4
le_orse <damien.messner@gmail.com>
2017-09-17 00:00:26 +0200

    fin videoinfo

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,12 +1,19 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
 const {app, BrowserWindow, ipcMain} = electron;
 
+//déclaration de mainWindow ici pour des histoires de scope
+let mainWindow;
+
 app.on('ready', ()=> {
-    const mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
-ipcMain.on('video:submit', () => {
-    console.log("oui c'est bon");
+ipcMain.on('video:submit', (event, data) => {
+    ffmpeg.ffprobe(data, (err, metadata)=>{
+        mainWindow.webContents.send('video:response', metadata.format.duration);
+        console.log("oui c'est bon: "+metadata.format.duration);
+    })
 })
