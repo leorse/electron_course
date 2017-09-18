663d4df9fdb8180c9635fb2906fc7adb1ebe0348
dams <damien.messner@gmail.com>
2017-09-18 18:02:12 +0200

    menu (en cours)

--- todos/index.js
+++ todos/index.js
@@ -7,12 +7,39 @@ let mainWindow;
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL("file://"+__dirname+"/main.html");
+    
     const mainMenu = Menu.buildFromTemplate(menuTemplate);
     Menu.setApplicationMenu(mainMenu);
 })
 
 const menuTemplate = [
     {
-        label: 'File'
+        label: 'File',
+        submenu: [{
+            label:'Ouvrir'
+        },{
+            label:'Quitter',
+            click(){
+                app.quit();
+            }
+        }
+    ]
+    },
+    {
+        label: 'Aide',
+        submenu: [{
+            label:'Dev',
+            click(){
+                mainWindow.webContents.openDevTools();
+            }
+        },{
+            label:'A propos'
+        }
+    ]
     }
 ];
+
+if(process.platform === 'darwin')
+{
+    menuTemplate.unshift({});
+}
