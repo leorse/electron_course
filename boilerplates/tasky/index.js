7b2cd3aee57a4f4481dc561bed3d1671f8c57dcc
leorse <damien.messner@gmail.com>
2017-09-25 21:12:29 +0200

    correction

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -19,7 +19,8 @@ app.on('ready', () => {
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
     tray = new Tray(iconPath);
 
-    tray.on('click', ()=>{
+    tray.on('click', ()=>
+    {
         if(mainWindow.isVisible())
         {
             mainWindow.hide();
