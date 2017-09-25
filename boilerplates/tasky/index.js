a3fa0486dc43fd351e819abfebdfa074e15be244
leorse <damien.messner@gmail.com>
2017-09-25 23:19:15 +0200

    ipc entre react et electron

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -1,21 +1,17 @@
 const electron = require('electron');
 const path = require('path');
-const {app, BrowserWindow, Tray} = electron;
+const {app, ipcMain} = electron;
 const TimerTray = require('./app/timer_tray');
+const MainWindow = require('./app/mainwindow');
 
 
 let mainWindow;
 let tray;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        height:500,
-        width:300,
-        frame:false,
-        resizable:false,
-        show:false
-    });
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+    //uniqmenet sur OS
+    //app.dock.hide();
+    mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
 
     const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
@@ -24,4 +20,9 @@ app.on('ready', () => {
     tray = new TimerTray(iconPath, mainWindow);
 
 
+});
+
+ipcMain.on("update-timer", (event, timeleft) =>
+{
+    tray.setTitle(timeleft);
 });
